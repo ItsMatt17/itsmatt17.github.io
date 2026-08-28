6f570d5b8da19c36c6952e4d3fe3b3d606955c97
Matthew Payne <mattspycrafter@gmail.com>
2026-08-28 01:40:42 -0400

    fix: Links tab index on wrong tag

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -10,13 +10,14 @@ function SocialButton({
     <a
       href={href}
       target={target}
+      tabIndex={0}
       rel="noopener"
       className="group relative flex items-center gap-x-2 rounded-md bg-neutral-200 p-2 px-4 transition duration-100 ease-in-out focus:bg-neutral-400 focus:-translate-y-1 hover:-translate-y-1 active:bg-neutral-400 hover:bg-neutral-400"
       onClick={(e) => onClick(e)}
       ng-href={name}
     >
       <div className="max-h-10 max-w-10">
-        <img tabIndex={0} className="h-auto w-full object-cover" src={icon} alt={`Logo for ${name}.`} />
+        <img className="h-auto w-full object-cover" src={icon} alt={`Logo for ${name}.`} />
       </div>
       <p>{name}</p>
       {children}
